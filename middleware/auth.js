5eff1f6bb01662ee7d1733173ce5441dbbb07eb3
nntnvlogs <lebaobinh20036@gmail.com>
2025-12-26 15:40:42 +0700

    add auth middle

---
+++ middleware/auth.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1]; // Bearer <token>
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user; // gán thông tin user vào request
+    next();
+  });
+}
+
+module.exports = authenticateToken;
