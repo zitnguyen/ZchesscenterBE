419c3eefc2b606bee0e2467eed878a7296b72fb7
nntnvlogs <lebaobinh20036@gmail.com>
2025-12-26 14:58:04 +0700

    add new gv

--- routers/Teachers.js
+++ routers/Teachers.js
@@ -10,3 +10,20 @@ router.get("/", async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 });
+// thêm gv mới
+router.post("/", async (req, res) => {
+    try{
+        const teacher = new Teacher({
+            name:req.body.name,
+            email:req.body.email,
+            role: req.body.role
+    });
+    try{
+        const newTecher = await teacher.save();
+        res.status(201).json(newTecher);
+    }
+    catch(err){
+        res.status(400).json({message: err.message});
+    }
+    });
+    
